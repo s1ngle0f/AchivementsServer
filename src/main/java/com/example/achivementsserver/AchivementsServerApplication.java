d79d587438954f48d1bd3fdf0a8f38905e7df471
s1ngle0f <zubkovmihail12345@gmail.com>
2023-09-16 16:45:56 +0300

    Initial commit

---
+++ src/main/java/com/example/achivementsserver/AchivementsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.achivementsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchivementsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AchivementsServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/achivementsserver/AchivementsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.achivementsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchivementsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
